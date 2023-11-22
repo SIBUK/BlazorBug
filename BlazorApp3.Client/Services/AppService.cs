e268b527a4b8448f123c5a9727e5964a76a65faa
Simon Bailey <sibuk2000@gmail.com>
2023-11-22 22:36:56 +0000

    Init
    Init

---
+++ BlazorApp3.Client/Services/AppService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp3.Client.Services
+{
+    public class AppService
+    {
+        HttpClient _httpClient;
+
+        public AppService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
